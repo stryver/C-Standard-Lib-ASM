6a763e23c7d261ee357a12a0059ff0d54661261e
GaulthierBOggio <gaulthier.boggio@epitech.eu>
2023-09-01 19:53:02 +0200

    clean main.c

--- main.c
+++ main.c
@@ -7,19 +7,6 @@ extern char* _my_strcat(char* dest, const char* src);
 
 int main() {
     //STRLEN
-    const char *str = "Hello World !";
-    size_t length = _my_strlen(str);
-    printf("---STRLEN---\n");
-    printf("La longueur de la chaîne est : %zu\n\n", length);
-    //STRCPY
-    char dest[100];
-    _my_strcpy(dest, "I love raccoons");
-    printf("---STRCPY---\n");
-    printf("La chaîne copiée est : %s\n\n", dest);
-    //STRCAT
-    char dest2[100] = "Hello ";
-    _my_strcat(dest2, "World !");
-    printf("---STRCAT---\n");
-    printf("La chaîne concaténée est : %s\n\n", dest2);
+
     return 0;
 }
